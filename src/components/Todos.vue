5386a78e83f70203d131cdcf0b3b43ed509c1650
Theophile <Noel>
2019-06-26 12:41:52 +0200

    initial commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+       <div>
+           <div v-bind:key="todo.id" v-for="todo in todos">
+               <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"/>
+           </div>
+       </div>
+    </div>
+</template>
+
+<script>
+
+import TodoItem from './TodoItem.vue';
+export default {
+    name: "Todos",
+    props: ["todos"],
+    components: {
+        TodoItem
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>This is the about page of a wonderful useless app</p>
+  </div>
+</template>
